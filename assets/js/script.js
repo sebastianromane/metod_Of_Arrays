52ff1310b70e46b8b6315ff95addf6b8f45d9ba3
sebastianromane <sebastianromane@gmail.com>
2022-07-31 22:15:25 -0400

    declaration of variables.

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+//declaration of variables.
+const listTask = document.getElementById("listTask"); //listado de tareas
+const inputTask = document.getElementById("inputTask"); //input del html
+const button = document.getElementById("button"); //agrega tareas
+const countTask = document.getElementById("countTask"); //cuenta tareas
+const taskComplete = document.getElementById("taskComplete"); //cuenta tareas completadas
